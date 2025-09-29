8fa2a74888dcd58c490d6ed2ff32a69f549258ca
Edgar Medina <edgrmdna@gmail.com>
2025-09-28 22:29:15 -0700

    added data as geojson instead of csv.

--- js/map.js
+++ js/map.js
@@ -1,28 +1,30 @@
+//imports
+//import Papa from 'papaparse';
 // Global variables
 let map;
-// path to csv data
-let path = "data/occurences.csv";
 // CSV Container
 let markers = L.featureGroup();
 
 // initialize
 $( document ).ready(function() {
     createMap();
-	readCSV(path)
+	//readCSV(path)
 });
 
 // create the map
 function createMap(){
 	map = L.map('map').setView([33.80100,-118.205], 13);
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/edgrmdna/cj0vm58cc00c32rnyk5c7xohw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRncm1kbmEiLCJhIjoiRV8wRG1URSJ9.-Gjqcw0AmLxIaGP10UuGqg ', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	//L.tileLayer('https://api.mapbox.com/styles/v1/edgrmdna/cj0vm58cc00c32rnyk5c7xohw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRncm1kbmEiLCJhIjoiRV8wRG1URSJ9.-Gjqcw0AmLxIaGP10UuGqg ', 
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',	
+		{ attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 }
 
+/*
 // function to read csv data
 function readCSV(){
-	Papa.parse(path, {
+		Papa.parse(path, {
 		header: true,
 		download: true,
 		complete: function(data) {
@@ -34,7 +36,8 @@ function readCSV(){
 		}
 	});
 }
-
+*/
+/*
 function mapCSV(data){
 	
 	// circle options
@@ -61,3 +64,75 @@ function mapCSV(data){
 	// fit markers to map
 	map.fitBounds(markers.getBounds())
 }
+*/
+
+async function addExternalGeoJson() {
+    const response = await fetch("data/boundary.geojson");
+    const data = await response.json();
+    L.geoJSON(data).addTo(map);
+	L.geoJSON()
+
+}
+addExternalGeoJson();
+
+// Plant symbols:
+var plantMarkerOptions = {
+        radius: 8,
+        fillColor: "#57DE79",
+        color: "#000",
+        weight: 1,
+        opacity: 0.45,
+        fillOpacity: 0.8
+    };
+
+async function addPlantGeoJson(){
+	const response = await fetch("data/Plantae_Kingdom.geojson");
+	const data_plant = await response.json();
+	L.geoJSON(data_plant, {
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, plantMarkerOptions);
+		}
+	}).addTo(map);
+}
+addPlantGeoJson();
+
+// Animal symbols:
+var animalMarkerOptions = {
+        radius: 8,
+        fillColor: "#F77259",
+        color: "#000",
+        weight: 1,
+        opacity: 0.45,
+        fillOpacity: 0.8
+    };
+
+async function addAnimalGeoJson(){
+	const response = await fetch("data/Animalia_Kingdom.geojson");
+	const data_plant = await response.json();
+	L.geoJSON(data_plant, {
+		pointToLayer:  function (feature, latlng) {
+			return L.circleMarker(latlng, animalMarkerOptions);
+		}
+	}).addTo(map);
+}
+addAnimalGeoJson();
+
+// Plant symbols:
+var fungiMarkerOptions = {
+        radius: 8,
+        fillColor: "#8C6E41",
+        color: "#000",
+        weight: 1,
+        opacity: 0.45,
+        fillOpacity: 0.8
+    };
+async function addFungiGeoJson(){
+	const response = await fetch("data/Fungi_Kingdom.geojson");
+	const data_plant = await response.json();
+	L.geoJSON(data_plant, {
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, fungiMarkerOptions);
+		}
+	}).addTo(map);
+}
+addFungiGeoJson();
