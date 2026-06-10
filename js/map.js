3980601889ebfa441616363577ea3c4ff9a2f94a
Edgar Medina <edgrmdna@gmail.com>
2026-06-09 21:56:56 -0700

    Switch tile layer source to Mapbox

--- js/map.js
+++ js/map.js
@@ -27,8 +27,8 @@ $(document).ready(function() {
 function createMap(){
     map = L.map('map').setView([33.96500,-118.300], 11);
 
-    //L.tileLayer('https://api.mapbox.com/styles/v1/edgrmdna/cj0vm58cc00c32rnyk5c7xohw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRncm1kbmEiLCJhIjoiRV8wRG1URSJ9.-Gjqcw0AmLxIaGP10UuGqg ', 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',    
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',  
+    L.tileLayer('https://api.mapbox.com/styles/v1/edgrmdna/cj0vm58cc00c32rnyk5c7xohw/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZWRncm1kbmEiLCJhIjoiRV8wRG1URSJ9.-Gjqcw0AmLxIaGP10UuGqg ',   
         { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 };
